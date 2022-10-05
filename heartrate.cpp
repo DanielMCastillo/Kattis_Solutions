26b2bea33dc96ca01450e41ae5a8719f0c7dfb01
Daniel Castillo <danielalejandromoralescastillo@gmail.com>
2022-10-04 22:52:56 -0500

    Heart Rate

---
+++ heartrate.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  while (n--) {
+    int b;
+    double p;
+    cin >> b >> p;
+    double bpm = 60 * b / p;
+    double diff = 60 / p;
+    cout << fixed << setprecision(4);
+    cout << bpm - diff << " " << bpm << " " << bpm + diff << endl;
+  }
+  return 0;
+}
