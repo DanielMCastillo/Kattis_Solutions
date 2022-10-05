53f93442a2d44520f6ee886a0fd1017f972d954e
Daniel Castillo <danielalejandromoralescastillo@gmail.com>
2022-10-04 22:52:36 -0500

    Zamka

---
+++ zamka.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int dsum(int n) {
+  int sum = 0;
+  while (n > 0) {
+    sum += n % 10;
+    n /= 10;
+  }
+  return sum;
+}
+
+int main() {
+  int l, d, x;
+  cin >> l >> d >> x;
+  for (int n = l; n <= d; n++) {
+    if (dsum(n) == x) {
+      cout << n << endl;
+      break;
+    }
+  }
+  for (int m = d; m >= l; m--) {
+    if (dsum(m) == x) {
+      cout << m << endl;
+      break;
+    }
+  }
+  return 0;
+}
