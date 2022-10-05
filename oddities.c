46b16a2e81f6d6845973791b15194507a907792c
Daniel Castillo <danielalejandromoralescastillo@gmail.com>
2022-10-04 22:53:39 -0500

    Oddities

---
+++ oddities.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    scanf("%d",&n);
+
+    while (n--)
+    {   
+        int x;
+        scanf("%d",&x);
+
+        if (x%2==0){
+            printf("%d is even\n",x);
+        }
+        else
+        {
+            printf("%d is odd\n", x);
+        }
+    }
+    return 0;
+}
