b7483bd40e5bca42348a40c9523f5dca5dfbdbc2
Daniel Castillo <danielalejandromoralescastillo@gmail.com>
2022-10-04 22:53:22 -0500

    Trik

---
+++ trik.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+  string moves;
+  cin >> moves;
+  vector<int> cups = {1, 0, 0};
+  for (int i = 0; i < moves.size(); i++) {
+    if (moves[i] == 'A') {
+      swap(cups[0], cups[1]);
+    } else if (moves[i] == 'B') {
+      swap(cups[1], cups[2]);
+    } else {
+      swap(cups[0], cups[2]);
+    }
+  }
+  for (int i = 0; i < 3; i++) {
+    if (cups[i] == 1) {
+      cout << i + 1 << endl;
+    }
+  }
+  return 0;
+}
