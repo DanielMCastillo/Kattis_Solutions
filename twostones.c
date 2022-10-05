741453f58c473f96571f1f1f213de278a04db649
Daniel Castillo <danielalejandromoralescastillo@gmail.com>
2022-10-04 22:55:25 -0500

    Take Two Stones

---
+++ twostones.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){ 
+
+    int s;
+
+    scanf("%d", &s);
+
+    if(s%2==0){
+        printf("Bob\n");
+    }else{
+        printf("Alice\n");
+    }
+
+
+
+    return 0;
+}
